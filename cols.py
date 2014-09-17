ec79a4bf50e5ce101ee416c8109e5492a93280d0
Kyle Schomp <kyle@pc079088.(none)>
2014-09-17 16:07:00 +0200

    updated cols and set to be a bit more user friendly

--- cols.py
+++ cols.py
@@ -1,29 +1,47 @@
 #!/usr/bin/python
 
+import logging
+import argparse
 import sys
+import traceback
 import os
-import re
-import urllib
 
-def test():
-	items = set()
-	index = []
-	for v in sys.argv[1:len(sys.argv)]:
-		index.append(int(v))
-#        f = open(sys.argv[2], "r")
-#        for line in f:
-	while 1:
-#	  try:
-		line = sys.stdin.readline()
-                if not line:
-                        break
+def columns(infile, outfile, cols, delimiter):
+    jdelim = delimiter if delimiter != None else ' '
+    for line in infile:
+        try:
+	    chunks = line.rstrip().split(delimiter)
+	    outfile.write(jdelim.join([chunks[i] for i in cols])+'\n')
+	except Exception as e:
+            logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
 
-		chunk = line.rstrip().split()
-		print " ".join([chunk[i] for i in index])
-#	  except:
-#		cept = 1
+def main():
+    columns(args.infile, args.outfile, args.columns, args.delimiter)
 
-#	f.close()
+if __name__ == "__main__":
+    # set up command line args
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
+                                     description='Select columns from a table file.')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-c', '--columns', nargs='+', type=int, default='0')
+    parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
+    args = parser.parse_args()
 
-test()
+    # set up logging
+    if args.quiet:
+        level = logging.WARNING
+    elif args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(
+        format = "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
+        level = level
+    )
+
+    main()
+
 
--- set.py
+++ set.py
@@ -1,31 +1,48 @@
 #!/usr/bin/python
 
+import logging
+import argparse
 import sys
+import traceback
 import os
-import re
-import urllib
 
-def test():
-	items = set()
-	index = int(sys.argv[1])
-#        f = open(sys.argv[2], "r")
-#        for line in f:
-	while 1:
-	  try:
-		line = sys.stdin.readline()
-                if not line:
-                        break
+def formset(infile, outfile, index, delimiter):
+    items = set()
+    for line in infile:
+        try:
+    	   chunk = line.split(delimiter)[index].rstrip()
+	   if chunk not in items:
+	      items.add(chunk)
+	      outfile.write(chunk+'\n')
+	except Exception as e:
+           logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
 
-		chunk = line.split()[index].rstrip()
-		items.add(chunk)
-	  except:
-		cept = 1
+def main():
+    formset(args.infile, args.outfile, args.column, args.delimiter)
 
-#	f.close()
+if __name__ == "__main__":
+    # set up command line args
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
+                                     description='Compute the set of strings from a column in files.')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-c', '--column', type=int, default=0)
+    parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
+    args = parser.parse_args()
 
-	for chunk in items:
-		print chunk
-		
+    # set up logging
+    if args.quiet:
+        level = logging.WARNING
+    elif args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(
+        format = "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
+        level = level
+    )
 
-test()
+    main()
 
