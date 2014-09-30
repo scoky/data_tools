5327789940d2b284045cfc3d187e183ce198470b
Kyle Schomp <kgs7@case.edu>
2014-09-30 17:11:53 +0200

    added methods for performing a lambda upon each column, row in a file

--- by-col.py
+++ by-col.py
@@ -6,23 +6,39 @@ import sys
 import traceback
 import os
 
-def by_row(infile, outfile, op):
+def by_col(infile, outfile, op, delimiter):
+    cols = []
+    row = 0
     for line in infile:
         try:
-	    outfile.write(op(line))
+	    chunks = line.rstrip().split(delimiter)
+	    while len(chunks) > len(cols):
+		cols.append([])
+	    for i in range(len(cols)):
+		while len(cols[i]) < row:
+			cols[i].append(None)
+		if len(chunks) > i:
+			cols[i].append(chunks[i])
+		else:
+			cols[i].append(None)
+	    row += 1
 	except Exception as e:
             logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
 
+    for col in cols:
+	outfile.write(op(col))
+
 def main():
-    by_row(args.infile, args.outfile, eval(args.operation))
+    by_col(args.infile, args.outfile, eval(args.operation), args.delimiter)
 
 if __name__ == "__main__":
     # set up command line args
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
-                                     description='Perform an operation upon each row.')
-    parser.add_argument('operation', help='lambda expression that takes a single argument row.')
+                                     description='Perform an operation upon each column.')
+    parser.add_argument('operation', help='lambda expression that takes a single argument column as array.')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-d', '--delimiter', default=None)
     parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
     args = parser.parse_args()
