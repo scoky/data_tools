a2e26e97be223e6f38c36d102e0d4a1cce1a822b
Kyle Schomp <bkyle@empire01.(none)>
2014-10-03 16:43:32 +0200

    fixed bugs in the O2 parser

--- log_parsing/o2_parser.py
+++ log_parsing/o2_parser.py
@@ -10,16 +10,17 @@ from parse_logs import LineParser
 
 class O2Parser(LineParser):
 	def __init__(self):
-		self.compress = re.compile('(\d+)\s*->\s*(\d+)\s+(\d+)%\s*(\w+)')
+		self.compress = re.compile('(\d+)\s*->\s*(\d+)\s+(-?\d+)%\s*(\w+)')
 		pass
 
 	def parse(self, line):
 		chunks = line.rstrip().split()
 		url = chunks[-1].strip('|')
 		domain = urlparse(url).netloc
-		if not domain.endswith('googlevideo.com') and not domain.endswith('facebook.com')
+		if not domain.endswith('googlevideo.com') and not domain.endswith('facebook.com'):
 			return EmptyLine()
 		
+		pre_size = post_size = ratio = hit = '?'
 		match = self.compress.search(line)
 		if match:
 			pre_size = match.group(1)
@@ -27,7 +28,7 @@ class O2Parser(LineParser):
 			ratio = match.group(3)
 			hit = 'HIT' in match.group(4)
 		
-		return O2Line(url, pre_size, post_size, ratio, hit)
+		return O2Line(domain, pre_size, post_size, ratio, hit)
 
 class O2Line(object):
 	def __init__(self, url, pre_size, post_size, ratio, hit):
@@ -38,7 +39,7 @@ class O2Line(object):
 		self.hit = hit
 
 	def output(self):
-		return " ".join(self.url, self.pre_size, self.post_size, self.ratio, str(self.hit))
+		return self.url+' '+self.pre_size+' '+self.post_size+' '+self.ratio+' '+str(self.hit)+'\n'
 
 class EmptyLine(object):
 	def output(self):
