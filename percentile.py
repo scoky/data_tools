8c438b8490b753294ab9dea9fb11bc0c092ddf9d
Kyle Schomp <kgs7@case.edu>
2014-11-07 16:59:28 +0100

    improved the percentile package

--- percentile.py
+++ percentile.py
@@ -1,38 +1,61 @@
 #!/usr/bin/python
 
-import sys
 import os
-from math import sqrt
-from math import pow
+import sys
+import logging
+import argparse
+import traceback
+from decimal import Decimal
 
-def test():
-	cur = None
-	colRow = sys.argv[1]
-	ptile = int(sys.argv[2])
-	index = int(sys.argv[3]) # starting index for row, col otherwise
+def percentile(rows, pts=[0, 0.25, 0.5, 0.75, 100], keys=0, values=0):
+    maximum = rows[-1][values]
+    pt = 0
+    for r in rows:       
+       while r[values] >= pts[pt]*maximum:
+          yield r[keys]
+          pt += 1
+          if pt >= len(pts):
+             break
+    while pt < len(pts):
+       yield rows[-1][keys]
+       pt += 1
 
-	x = []
-	while 1:
-                line = sys.stdin.readline()
-                if not line:
-                        break
+def percentileFile(infile, outfile, pts=[0, 0.25, 0.5, 0.75, 100], keys=0, values=0, delimiter=None):
+    rows = []
+    for line in infile:
+        try:
+    	   chunks = line.rstrip().split(delimiter)
+	   rows.append([chunks[keys], float(chunks[values])])
+	except Exception as e:
+           logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
+    for val in percentile(rows, pts=pts, keys=0, values=1):
+	outfile.write(val+'\n')
 
-		chunks = line.rstrip().split()
-		if colRow == "row":
-			vals = chunks[index:len(chunks)]
-			x = []
-			for v in vals:
-				x.append(float(v))
-			x = sorted(x)
-			tile = x[int(len(x)*(float(ptile)/100.0))]
-			print " ".join(chunks[0:index]), tile
-		else:
-			x.append(float(chunks[index]))
+if __name__ == "__main__":
+    # set up command line args
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
+                                     description='Compute the percentiles of a two column format')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-k', '--keys', type=int, default=0)
+    parser.add_argument('-a', '--values', type=int, default=0)
+    parser.add_argument('-p', '--percentiles', nargs='+', type=float, default=[0, 0.25, 0.5, 0.75, 100])
+    parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
+    args = parser.parse_args()
 
-	if colRow == "col":
-		x = sorted(x)
-        	tile = x[int(len(x)*(float(ptile)/100.0))]
-	        print tile
+    # set up logging
+    if args.quiet:
+        level = logging.WARNING
+    elif args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(
+        format = "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
+        level = level
+    )
 
-test()
+    percentileFile(args.infile, args.outfile, args.percentiles, args.keys, args.values, args.delimiter)
 
