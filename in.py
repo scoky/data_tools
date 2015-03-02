ae807972fd117d22dd9eba488886b143d07c430c
Kyle Schomp <kgs7@case.edu>
2015-03-02 14:59:58 -0500

    new version of in.py

--- in.py
+++ in.py
@@ -7,38 +7,39 @@ import argparse
 import traceback
 from input_handling import findNumber
 
-def compareIn(infile, outfile, column, elements, testIn=True, delimiter=None):
-	for line in infile:
-	   try:
-		c = line.rstrip().split(delimiter)[column]
-		if (testIn and c in elements) or (not testIn and c not in elements):
-			outfile.write(line)
-	   except Exception as e:
-           	logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
+def compareIn(infile, outfile, columns, elements, testIn=True, delimiter=None):
+    jdelim = delimiter if delimiter else ' '
+    for line in infile:
+        try:
+            chunks = line.rstrip().split(delimiter)
+            c = jdelim.join([chunks[i] for i in columns])
+            if (testIn and c in elements) or (not testIn and c not in elements):
+                outfile.write(line)
+        except Exception as e:
+            logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
 
 def main():
     elements = set(args.list)
     if args.listfiles:
-       for listfile in args.listfiles:
-	  with open(listfile, 'r') as f:
-	     for line in f:
-		elements.add(line.rstrip())
+        for listfile in args.listfiles:
+            with open(listfile, 'r') as f:
+                for line in f:
+                    elements.add(line.rstrip())
 
 
-    compareIn(args.infile, args.outfile, args.column, elements, not args.notin, args.delimiter)
+compareIn(args.infile, args.outfile, args.columns, elements, not args.notin, args.delimiter)
 
 if __name__ == "__main__":
     # set up command line args
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
-                                     description='Output rows where column is in list')
+                                     description='Output rows where column(s) is in list')
     parser.add_argument('list', nargs='*', help='List of elements')
     parser.add_argument('-l', '--listfiles', nargs='+', help='Files containing list elements, one per line.')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-c', '--column', type=int, default=0)
+    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
     parser.add_argument('-n', '--notin', action='store_true', default=False)
     parser.add_argument('-d', '--delimiter', default=None)
-#    parser.add_argument('-f', '--findNumber', action='store_true', default=False, help='find number in column values')
     parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
     args = parser.parse_args()
