a63bf3ac600e092ba38bf3338c24c3301c4d23b0
Kyle Schomp <kgs7@case.edu>
2015-03-07 13:51:34 -0500

    changed cdf.py to reduce memory footprint

--- cdf.py
+++ cdf.py
@@ -14,19 +14,15 @@ def cdf(rows, keys=0, values=1):
        total += r[values] 
        yield [r[keys], total]    
 
-def cdfFile(infile, outfile, keys=0, values=1, delimiter=None):
+def cdfFile(infile, keys=0, values=1, delimiter=None):
     total = Decimal(0)
-    rows = []
     for line in infile:
         try:
-    	   chunks = line.rstrip().split(delimiter)
-	   rows.append([chunks[keys], findNumber(chunks[values])])
-	except Exception as e:
-           logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
-
-    jdelim = delimiter if delimiter != None else ' '
-    for val in cdf(rows, keys=0, values=1):
-	outfile.write(val[0]+jdelim+str(val[1])+'\n')
+            chunks = line.rstrip().split(delimiter)
+            total += findNumber(chunks[values])
+            yield [chunks[keys], total]
+        except Exception as e:
+            logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
 
 if __name__ == "__main__":
     # set up command line args
@@ -37,21 +33,9 @@ if __name__ == "__main__":
     parser.add_argument('-k', '--keys', type=int, default=0)
     parser.add_argument('-a', '--values', type=int, default=1)
     parser.add_argument('-d', '--delimiter', default=None)
-    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
     args = parser.parse_args()
 
-    # set up logging
-    if args.quiet:
-        level = logging.WARNING
-    elif args.verbose:
-        level = logging.DEBUG
-    else:
-        level = logging.INFO
-    logging.basicConfig(
-        format = "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
-        level = level
-    )
-
-    cdfFile(args.infile, args.outfile, keys=args.keys, values=args.values, delimiter=args.delimiter)
+    jdelim = args.delimiter if args.delimiter != None else ' '
+    for key,value in cdfFile(args.infile, keys=args.keys, values=args.values, delimiter=args.delimiter):
+        args.outfile.write(key + jdelim + str(value) + '\n')
 
