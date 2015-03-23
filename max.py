53dcec5db307cdcdc5c4ee9bda27ebe8431677b5
Kyle Schomp <kgs7@case.edu>
2015-03-23 11:33:17 -0400

    new version of max with added -a option

--- max.py
+++ max.py
@@ -12,16 +12,23 @@ class MaxGroup(Group):
     def __init__(self, tup):
         super(MaxGroup, self).__init__(tup)
         self.maxes = [Decimal('-Inf')]*len(args.columns)
+        self.rows = [[]]*len(args.columns)
 
     def add(self, chunks):
         vals = [findNumber(chunks[i]) for i in args.columns]
-        self.maxes = map(max, self.maxes, vals)
+        for i,v in enumerate(vals):
+            if v > self.maxes[i]:
+                self.maxes[i] = v
+                self.rows[i] = chunks
 
     def done(self):
         jdelim = args.delimiter if args.delimiter != None else ' '
         if len(self.tup) > 0:
             args.outfile.write(jdelim.join(self.tup) + jdelim)
-        args.outfile.write(jdelim.join(map(str, self.maxes)) + '\n')
+        if args.append:
+            args.outfile.write('\n'.join([jdelim.join(chunks) for chunks in self.rows]) + '\n')
+        else:
+            args.outfile.write(jdelim.join(map(str, self.maxes)) + '\n')
 
 if __name__ == "__main__":
     # set up command line args
@@ -32,6 +39,7 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
     parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-a', '--append', action='store_true', default=False, help='append result to columns')
     args = parser.parse_args()
 
     grouper = UnsortedInputGrouper(args.infile, MaxGroup, args.group, args.delimiter)
