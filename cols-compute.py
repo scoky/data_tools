dffbc0c3dc2aadbdc0104a3cf1e7e0905cfcd928
Kyle Schomp <kgs7@case.edu>
2015-03-27 14:03:20 -0400

    large changes to cols-compute and where. in particular, now they work between rows

--- cols-compute.py
+++ cols-compute.py
@@ -1,53 +1,117 @@
 #!/usr/bin/python
 
-import logging
-import argparse
-import sys
-import traceback
 import os
 import re
+import sys
+import argparse
+import traceback
 from input_handling import findNumber
+from group import Group,UnsortedInputGrouper
 
-def compute(infile, outfile, expression, append=False, delimiter=None):
-    jdelim = delimiter if delimiter != None else ' '
-    # Pattern to pull out integers which represent columns
-    pattern = re.compile("(c\[\d+\])")
-    # Replace integers with indices into an array and convert to a lambda expression
-    expression = eval('lambda c: '+ pattern.sub(r'findNumber(\1)', args.expression))
-    for line in infile:
-        chunks = line.rstrip().split(delimiter)
-        try:    
-	    # Evaluate lambda with the row input from the file
-	    if append:
-	       outfile.write('%s%s' % (line.rstrip(), jdelim))
-	    outfile.write('%s\n' % expression(chunks))
-    	except Exception as e:
-            logging.error('Error on input: %s%s\n%s', line, e, traceback.format_exc())
+class Compute3Group(Group):
+    def __init__(self, tup):
+        super(Compute3Group, self).__init__(tup)
+        self.rows = (None, None, None)
+
+    def add(self, chunks):
+        self.rows = (self.rows[1], self.rows[2], chunks)
+        if not self.rows[1]:
+            return
+        if len(self.tup) > 0:
+            args.outfile.write(args.jdelim.join(self.tup) + args.jdelim)
+        if args.append:
+            args.outfile.write(args.jdelim.join(self.rows[1]) + args.jdelim)
+        if all(self.rows):
+            args.outfile.write(str(args.expression(self.rows[0], self.rows[1], self.rows[2])) + '\n')
+        else:
+            args.outfile.write('None\n')
+
+    def done(self):
+        self.add(None)
+        
+class ComputePrevGroup(Group):
+    def __init__(self, tup):
+        super(ComputePrevGroup, self).__init__(tup)
+        self.rows = (None, None)
+
+    def add(self, chunks):
+        self.rows = (self.rows[1], chunks)
+        if len(self.tup) > 0:
+            args.outfile.write(args.jdelim.join(self.tup) + args.jdelim)
+        if args.append:
+            args.outfile.write(args.jdelim.join(self.rows[1]) + args.jdelim)
+        if all(self.rows):
+            args.outfile.write(str(args.expression(self.rows[0], self.rows[1])) + '\n')
+        else:
+            args.outfile.write('None\n')
+
+    def done(self):
+        pass
+        
+class ComputeNextGroup(Group):
+    def __init__(self, tup):
+        super(ComputeNextGroup, self).__init__(tup)
+        self.rows = (None, None)
+
+    def add(self, chunks):
+        self.rows = (self.rows[1], chunks)
+        if not self.rows[0]:
+            return
+
+        if len(self.tup) > 0:
+            args.outfile.write(args.jdelim.join(self.tup) + args.jdelim)
+        if args.append:
+            args.outfile.write(args.jdelim.join(self.rows[0]) + args.jdelim)
+        if all(self.rows):
+            args.outfile.write(str(args.expression(self.rows[0], self.rows[1])) + '\n')
+        else:
+            args.outfile.write('None\n')
+
+    def done(self):
+        self.add(None)
+        
+class Compute1Group(Group):
+    def __init__(self, tup):
+        super(Compute1Group, self).__init__(tup)
+
+    def add(self, chunks):
+        if len(self.tup) > 0:
+            args.outfile.write(args.jdelim.join(self.tup) + args.jdelim)
+        if args.append:
+            args.outfile.write(args.jdelim.join(chunks) + args.jdelim)
+        args.outfile.write(str(args.expression(chunks)) + '\n')
+
+    def done(self):
+        pass
 
 if __name__ == "__main__":
     # set up command line args
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
-                                     description='Compute the set of strings from a column in files.')
-    parser.add_argument('expression', help='equation to call. use c[i] to indicate column i.')
+                                     description='Compute maximum of column(s)')
+    parser.add_argument('expression', help='equation to call. use c[i] to indicate column i, p[i] to indicate column i of the previous row, and n[i] to indicate column i of the next row.')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
     parser.add_argument('-a', '--append', action='store_true', default=False, help='append result to columns')
-    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
     args = parser.parse_args()
-
-    # set up logging
-    if args.quiet:
-        level = logging.WARNING
-    elif args.verbose:
-        level = logging.DEBUG
+    
+    args.jdelim = args.delimiter if args.delimiter != None else ' '
+    # Pattern to pull out integers which represent columns
+    pattern = re.compile("([pcn]\[\d+\])")
+    # Replace integers with indices into an array and convert to a lambda expression
+    if 'n[' in args.expression and 'p[' in args.expression:
+        args.expression = eval('lambda p,c,n: '+ pattern.sub(r'findNumber(\1)', args.expression))
+        groupClass = Compute3Group
+    elif 'p[' in args.expression:
+        args.expression = eval('lambda p,c: '+ pattern.sub(r'findNumber(\1)', args.expression))
+        groupClass = ComputePrevGroup
+    elif 'n[' in args.expression:
+        args.expression = eval('lambda c,n: '+ pattern.sub(r'findNumber(\1)', args.expression))
+        groupClass = ComputeNextGroup
     else:
-        level = logging.INFO
-    logging.basicConfig(
-        format = "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
-        level = level
-    )
+        args.expression = eval('lambda c: '+ pattern.sub(r'findNumber(\1)', args.expression))
+        groupClass = Compute1Group
 
-    compute(args.infile, args.outfile, args.expression, args.append, args.delimiter)
-
+    grouper = UnsortedInputGrouper(args.infile, groupClass, args.group, args.delimiter)
+    grouper.group()
--- where.py
+++ where.py
@@ -3,11 +3,60 @@
 import os
 import re
 import sys
-import logging
 import argparse
 import traceback
 from input_handling import findNumber
-from decimal import Decimal
+from group import Group,UnsortedInputGrouper
+
+class Compute3Group(Group):
+    def __init__(self, tup):
+        super(Compute3Group, self).__init__(tup)
+        self.rows = (None, None, None)
+
+    def add(self, chunks):
+        self.rows = (self.rows[1], self.rows[2], chunks)
+        if all(self.rows) and args.expression(self.rows[0], self.rows[1], self.rows[2]):
+            args.outfile.write(args.jdelim.join(self.rows[1]) + '\n')
+
+    def done(self):
+        pass
+        
+class ComputePrevGroup(Group):
+    def __init__(self, tup):
+        super(ComputePrevGroup, self).__init__(tup)
+        self.rows = (None, None)
+
+    def add(self, chunks):
+        self.rows = (self.rows[1], chunks)
+        if all(self.rows) and args.expression(self.rows[0], self.rows[1]):
+            args.outfile.write(args.jdelim.join(self.rows[1]) + '\n')
+
+    def done(self):
+        pass
+        
+class ComputeNextGroup(Group):
+    def __init__(self, tup):
+        super(ComputeNextGroup, self).__init__(tup)
+        self.rows = (None, None)
+
+    def add(self, chunks):
+        self.rows = (self.rows[1], chunks)
+        if all(self.rows) and args.expression(self.rows[0], self.rows[1]):
+            args.outfile.write(args.jdelim.join(self.rows[0]) + '\n')
+
+    def done(self):
+        pass
+        
+class Compute1Group(Group):
+    def __init__(self, tup):
+        super(Compute1Group, self).__init__(tup)
+
+    def add(self, chunks):
+        if args.expression(chunks):
+            args.outfile.write(args.jdelim.join(chunks) + '\n')
+
+    def done(self):
+        pass
 
 def compare(infile, outfile, statement, delimiter):
     for line in infile:
@@ -19,29 +68,32 @@ if __name__ == "__main__":
     # set up command line args
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
                                      description='Output rows where comparator is true')
-    parser.add_argument('statement', default='c[0]>0', help='boolean statement to evaluate. use c[i] to indicate column i')
+    parser.add_argument('statement', default='c[0]>0', help='boolean statement to evaluate. use c[i] to indicate column i, p[i] to indicate column i of the previous row, and n[i] to indicate column i of the next row.')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
     parser.add_argument('-f', '--findNumber', action='store_true', default=False, help='find number in column values')
-    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
     args = parser.parse_args()
     if args.findNumber:
-        pattern = re.compile('(c\[\d+\])')
+        pattern = re.compile('([pcn]\[\d+\])')
         args.statement = pattern.sub(r'findNumber(\1)', args.statement)
-
-    # set up logging
-    if args.quiet:
-        level = logging.WARNING
-    elif args.verbose:
-        level = logging.DEBUG
+        
+    # Replace integers with indices into an array and convert to a lambda expression
+    if 'n[' in args.statement and 'p[' in args.statement:
+        args.expression = eval('lambda p,c,n: '+ args.statement)
+        groupClass = Compute3Group
+    elif 'p[' in args.statement:
+        args.expression = eval('lambda p,c: '+ args.statement)
+        groupClass = ComputePrevGroup
+    elif 'n[' in args.statement:
+        args.expression = eval('lambda c,n: '+ args.statement)
+        groupClass = ComputeNextGroup
     else:
-        level = logging.INFO
-    logging.basicConfig(
-        format = "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
-        level = level
-    )
+        args.expression = eval('lambda c: '+ args.statement)
+        groupClass = Compute1Group
 
-    compare(args.infile, args.outfile, args.statement, args.delimiter)
+    args.jdelim = args.delimiter if args.delimiter != None else ' '
+    grouper = UnsortedInputGrouper(args.infile, groupClass, args.group, args.delimiter)
+    grouper.group()
 
