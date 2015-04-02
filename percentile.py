2eecd11548172af00e343c3be6e0229351b5966d
Kyle Schomp <kgs7@case.edu>
2015-04-02 18:43:06 -0400

    allows for use with binned data

--- percentile.py
+++ percentile.py
@@ -4,6 +4,7 @@ import os
 import sys
 import argparse
 import traceback
+from collections import defaultdict
 from decimal import Decimal
 from input_handling import findNumber
 from group import Group,UnsortedInputGrouper
@@ -13,21 +14,23 @@ DEFAULT_PCT = map(Decimal, ['0', '0.01', '0.25', '0.5', '0.75', '0.99', '1'])
 class PercentileGroup(Group):
     def __init__(self, tup):
         super(PercentileGroup, self).__init__(tup)
-        self.rows = [[] for i in args.columns]
+        self.vals = defaultdict(int)
+        self.add = self.addBin if args.bin else self.addVal
 
-    def add(self, chunks):
-        vals = [findNumber(chunks[i]) for i in args.columns]
-        for v,r in zip(vals, self.rows):
-            r.append(v)
+    def addVal(self, chunks):
+        val = findNumber(chunks[args.column])
+        self.vals[val] += 1
+
+    def addBin(self, chunks):
+        val = findNumber(chunks[args.column])
+        b = int(chunks[args.bin])
+        self.vals[val] += b
 
     def done(self):
         jdelim = args.delimiter if args.delimiter != None else ' '
-        for c,p in zip(args.columns, percentile(self.rows, args.percentiles, range(len(self.rows)))):
-            if len(self.tup) > 0:
-                args.outfile.write(jdelim.join(self.tup) + jdelim)
-            if len(args.columns) > 1:
-                args.outfile.write(str(c) + jdelim)
-            args.outfile.write(jdelim.join(map(str, p)) + '\n')
+        if len(self.tup) > 0:
+            args.outfile.write(jdelim.join(self.tup) + jdelim)
+        args.outfile.write(jdelim.join(map(str, percentileDict(self.vals, args.percentiles))) + '\n')
 
 def percentile(rows, pts=DEFAULT_PCT, columns=[0]):
     for c in columns:
@@ -35,13 +38,24 @@ def percentile(rows, pts=DEFAULT_PCT, columns=[0]):
         indices = [int(pt*(len(rows[c])-1)) for pt in pts]
         yield [rows[c][index] for index in indices]
 
+def percentileDict(dic, pts=DEFAULT_PCT):
+    count = sum(dic.itervalues())
+    indices = [int(pt*(count-1)) for pt in pts]
+    count = 0
+    for k in sorted(dic):
+        count += dic[k]
+        while len(indices) > 0 and indices[0] < count:
+            yield k
+            indices.remove(indices[0])
+
 if __name__ == "__main__":
     # set up command line args
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
                                      description='Compute the percentiles of a two column format')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
+    parser.add_argument('-c', '--column', type=int, default=0)
+    parser.add_argument('-b', '--bin', type=int, default=None)
     parser.add_argument('-p', '--percentiles', nargs='+', type=Decimal, default=DEFAULT_PCT)
     parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
