89680741ea742705bd2a7d631fecd5ed187f1b62
Kyle Schomp <kgs7@case.edu>
2015-04-18 12:56:45 -0400

    clean up and new split command

--- split.py
+++ split.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python
 
-import sys
 import os
-import math
+import sys
+import logging
+import argparse
+import traceback
+from group import Group,UnsortedInputGrouper
+from log_parsing.file_handle_dictionary import FileHandleDict
 
-def test():
-       	lines = []
-       	filename = sys.argv[2]
-       	count = int(sys.argv[3])
-     	f = open(sys.argv[1], "r")
-        for line in f:
-		lines.append(line.rstrip())
+class SplitGroup(Group):
+    def __init__(self, tup):
+        super(SplitGroup, self).__init__(tup)
+        self.filename = args.file_prefix+'-'.join(tup)
+        self.jdelim = args.delimiter if args.delimiter != None else ' '
+        args.file_dict[self.filename] = open(self.filename, 'w')
 
-	max = int(math.ceil(float(len(lines)) / float(count)));
-	cur = 0
-	i = 1
-	f = open(filename+"."+str(i), "w")
-	for line in lines:
-		if cur >= max:
-			cur = 0
-			i += 1
-			f.close()
-			f = open(filename+"."+str(i), "w")
-		f.write(line+"\n")
-		cur += 1
+    def add(self, chunks):
+        if self.filename not in args.file_dict:
+            args.file_dict[self.filename] = open(self.filename, 'a')
+        args.file_dict[self.filename].write(self.jdelim.join(chunks) + '\n')
 
-test()
+    def done(self):
+        pass
+
+if __name__ == "__main__":
+    # set up command line args
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
+                                     description='Split a file on column(s).')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
+    args = parser.parse_args()
+    args.file_dict = FileHandleDict()
+    if args.infile == sys.stdin:
+        args.file_prefix = 'split-'
+    else:
+        args.file_prefix = args.infile.name+'.split-'
+
+    grouper = UnsortedInputGrouper(args.infile, SplitGroup, args.group, args.delimiter)
+    grouper.group()
 
