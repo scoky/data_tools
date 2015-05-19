4fb535b307e4755b566ebd7b34492556e7c005d9
Kyle Schomp <kgs7@case.edu>
2015-05-19 10:56:53 -0400

    new script for join files onto a file

---
+++ join.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+import traceback
+from collections import defaultdict
+
+if __name__ == "__main__":
+    # set up command line args
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
+                                     description='Join files on column(s) to first file')
+    parser.add_argument('infiles', nargs='*', type=argparse.FileType('r'), default=[sys.stdin])
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
+    parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-i', '--inner', action='store_true', default=False, help='inner merge')
+    args = parser.parse_args()
+    if len(args.columns) == 1:
+        args.columns = args.columns*len(args.infiles)
+    if len(args.columns) != len(args.infiles):
+        sys.stderr.write('InputError: invalid columns argument\n')
+        exit()
+        
+    jdelim = args.delimiter if args.delimiter != None else ' '
+    merge = defaultdict(str)
+    counts = defaultdict(int)
+    for i,tup in enumerate(zip(args.infiles[1:], args.columns[1:])):
+        infile,col = tup
+        for line in infile:
+            line = line.rstrip()
+            chunk = line.split(args.delimiter, col+1)[col]
+            merge[chunk] += '#JOIN#'+str(i) + jdelim + line + jdelim
+            counts[chunk] += 1
+        infile.close()
+
+    col = args.columns[0]
+    for line in args.infiles[0]:
+        line = line.rstrip()
+        chunk = line.split(args.delimiter, col+1)[col]
+        counts[chunk] += 1
+        if chunk in merge:
+            line += jdelim + merge[chunk]
+        if not args.inner or counts[chunk] == len(args.infiles):
+            args.outfile.write(line + '\n')
+
