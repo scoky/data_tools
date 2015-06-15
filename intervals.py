44d01a63c7faf44ab8bae52c0ed4a5d6661f8f45
Kyle Schomp <kgs7@case.edu>
2015-06-15 10:19:17 -0400

    updated interval script to consider first silent period as an interval

--- intervals.py
+++ intervals.py
@@ -11,14 +11,16 @@ from decimal import Decimal
 class IntervalGroup(Group):
     def __init__(self, tup):
         super(IntervalGroup, self).__init__(tup)
-        self.last = [None]*len(args.columns)
+        self.last = None
         self.jdelim = args.delimiter if args.delimiter != None else ' '
 
     def add(self, chunks):
-        vals = [findNumber(chunks[i]) for i in args.columns]
-        ints = [v-l if l != None else None for v,l in zip(vals,self.last)]
-        args.outfile.write(self.jdelim.join(chunks+map(str, ints)) + '\n')
-        self.last = vals
+        val = findNumber(chunks[args.column])
+        if not args.beginning:
+            args.beginning = val
+        diff = val - self.last if self.last != None else val - args.beginning
+        args.outfile.write(self.jdelim.join(chunks+[str(diff)]) + '\n')
+        self.last = val
 
     def done(self):
         pass
@@ -29,10 +31,11 @@ if __name__ == "__main__":
                                      description='Compute the difference between subsequent elements in a column')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
+    parser.add_argument('-c', '--column', type=int, default=0)
     parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
     args = parser.parse_args()
+    args.beginning = None
 
     grouper = UnsortedInputGrouper(args.infile, IntervalGroup, args.group, args.delimiter)
     grouper.group()
