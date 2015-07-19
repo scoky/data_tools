dd37f3f9176be56a8b229432e72f9207cca48f5b
Kyle Schomp <kgs7@case.edu>
2015-07-19 12:41:06 -0400

    new method for splines

--- resample_new.py
+++ resample_new.py
@@ -5,7 +5,7 @@ import sys
 import argparse
 import traceback
 from group import Group,run_grouping
-from scipy.interpolate import interp1d
+from scipy import interpolate
 import numpy as np
 
 class ResampleGroup(Group):
@@ -19,7 +19,7 @@ class ResampleGroup(Group):
         self.ypoints.append(float(chunks[args.ydata]))
 
     def done(self):
-        fit = interp1d(self.xpoints, self.ypoints, kind=args.interpolate, copy=False)
+        fit = args.interpolatef(self.xpoints, self.ypoints)
         ynew = fit(args.xnew)
         
         prefix = ''
@@ -27,6 +27,9 @@ class ResampleGroup(Group):
                 prefix = args.jdelim.join(self.tup) + args.jdelim
         for x,y in zip(args.xnew, ynew):
             args.outfile.write(prefix + str(x) + args.jdelim + str(y) + '\n')
+            
+def interp_linear(x, y)
+    return interp1d(x, x, kind=args.interpolate, copy=False)
 
 if __name__ == "__main__":
     # set up command line args
@@ -34,7 +37,7 @@ if __name__ == "__main__":
                                      description='Resample the data points with a different frequency')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-i', '--interpolate', choices=['linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic'], default='linear')
+    parser.add_argument('-i', '--interpolate', choices=['linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic', 'spline'], default='linear')
     parser.add_argument('-s', '--space', choices=['linear', 'log'], default='linear')
     parser.add_argument('-b', '--begin', type=float, default=None, help='value to begin resampling at')
     parser.add_argument('-t', '--terminate', type=float, default=None, help='value to terminate resampling at')
@@ -48,6 +51,11 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--ordered', action='store_true', default=False, help='input is sorted by group')
     args = parser.parse_args()
     args.jdelim = args.delimiter if args.delimiter != None else ' '
+    
+    if args.interpolate == 'spline':
+        args.interpolatef = interpolate.InterpolatedUnivariateSpline
+    else:
+        args.interpolatef = interp_linear
     
     if args.resample_file:
         args.xnew = []
