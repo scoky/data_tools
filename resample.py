36d03e186025f82e0167d0386d54b49fd66d69e0
Kyle Schomp <kgs7@case.edu>
2015-07-19 17:35:43 -0400

    updated resampling script

--- resample.py
+++ resample.py
@@ -5,80 +5,66 @@ import sys
 import argparse
 import traceback
 from decimal import Decimal,InvalidOperation
-from group import Group,UnsortedInputGrouper
+from group import Group,run_grouping
 
 class ResampleGroup(Group):
     def __init__(self, tup):
         super(ResampleGroup, self).__init__(tup)
         self.point = None
+
         self.jdelim = args.delimiter if args.delimiter != None else ' '
+        self.prefix = ''
+        if len(self.tup) > 0:
+            self.prefix = self.jdelim.join(self.tup) + self.jdelim
 
     def add(self, chunks):
         npoint = (Decimal(chunks[args.xdata]), Decimal(chunks[args.ydata]))
-        
-        if args.begin != None:
-            self.x = args.begin
-            while self.x < npoint[0]:
-                if len(self.tup) > 0:
-                    args.outfile.write(self.jdelim.join(self.tup) + self.jdelim)
-                args.outfile.write(str(self.x) + self.jdelim + str(npoint[1]) + '\n')
-                self.x += args.frequency
 
-        elif args.sync or (npoint[0] % args.frequency) == 0:
-            self.x = npoint[0]
+        # Set up iteration
+        if args.resample_file:
+            self.pt_generator = ( v for v in args.resample_values )
+        elif args.begin != None:
+            self.pt_generator = nextpoint(args.begin, args.terminate, args.frequency)
         else:
-            self.x = npoint[0] - (npoint[0] % args.frequency) + args.frequency
+            self.pt_generator = nextpoint(npoint[0], args.terminate, args.frequency)
+        # First sample point
+        try:
+            self.x = self.pt_generator.next()
+        except StopIteration:
+            self.x = Decimal('inf')
+
+        while self.x <= npoint[0]:
+            args.outfile.write(self.prefix + str(self.x) + self.jdelim + str(npoint[1]) + '\n')
+            try:
+                self.x = self.pt_generator.next()
+            except StopIteration:
+                self.x = Decimal('inf')
         self.point = npoint
-        if args.expand:
-            self.add = self.addAfterExpand
-        else:
-            self.add = self.addAfter
+
+        self.add = self.addAfter
 
     def addAfter(self, chunks):
-        if args.terminate and self.x > args.terminate:
-            self.add = self.addNothing
-            return
-
         npoint = (Decimal(chunks[args.xdata]), Decimal(chunks[args.ydata]))
-        if npoint[0] >= self.x:
-            y = args.interpolatef(self.point, npoint, self.x)
 
-            if len(self.tup) > 0:
-                args.outfile.write(self.jdelim.join(self.tup) + self.jdelim)
-            args.outfile.write(str(self.x) + self.jdelim + str(y) + '\n')
+        while self.x <= npoint[0]:
+            y = args.interpolatef(self.point, npoint, self.x)    
+            args.outfile.write(self.prefix + str(self.x) + self.jdelim + str(y) + '\n')
+            try:
+                self.x = self.pt_generator.next()
+            except StopIteration:
+                self.x = Decimal('inf')
 
-            if self.x != npoint[0] and (npoint[0] % args.frequency) == 0:
-                self.x = npoint[0]
-            else:
-                self.x = npoint[0] - (npoint[0] % args.frequency) + args.frequency
         self.point = npoint
-
-    def addAfterExpand(self, chunks):
-        npoint = (Decimal(chunks[args.xdata]), Decimal(chunks[args.ydata]))
-        if npoint[0] >= self.x:
-            # Unwind to compute first two points, so future points can be computed via deltas
-            y = args.interpolatef(self.point, npoint, self.x)
-            yd = args.interpolatef(self.point, npoint, self.x + args.frequency) - y
-            while npoint[0] >= self.x:
-                if args.terminate != None and self.x > args.terminate:
-                    self.add = self.addNothing
-                    return
-                if len(self.tup) > 0:
-                    args.outfile.write(self.jdelim.join(self.tup) + self.jdelim)
-                args.outfile.write(str(self.x) + self.jdelim + str(y) + '\n')
-                self.x += args.frequency
-                y += yd
-        self.point = npoint
-
-    def addNothing(self, chunks):
-        pass
 
     def done(self):
-        while args.terminate != None and self.x <= args.terminate:
-            if len(self.tup) > 0:
-                args.outfile.write(self.jdelim.join(self.tup) + self.jdelim)
-            args.outfile.write(str(self.x) + self.jdelim + str(self.point[1]) + '\n')
-            self.x += args.frequency
+        if self.x == Decimal('inf') or args.terminate == None:
+            return
+        while True:
+            args.outfile.write(self.prefix + str(self.x) + self.jdelim + str(self.point[1]) + '\n')
+            try:
+                self.x = self.pt_generator.next()
+            except StopIteration:
+                break
 
 
 def interp_linear(p1, p2, x):
@@ -87,8 +73,14 @@ def interp_linear(p1, p2, x):
     except InvalidOperation:
         return p1[1]   # p2[0] == p1[0]
 
-def interp_none(p1, p2, x):
+def interp_step(p1, p2, x):
     return p1[1]
+
+def nextpoint(b, t, f):
+    x = b
+    while t == None or x <= t:
+        yield x
+        x += f
 
 if __name__ == "__main__":
     # set up command line args
@@ -97,19 +89,24 @@ if __name__ == "__main__":
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     parser.add_argument('-f', '--frequency', type=Decimal, default=Decimal('1'))
-    parser.add_argument('-i', '--interpolate', choices=['linear', 'none'], default='linear')
-    parser.add_argument('-s', '--sync', action='store_true', default=False)
+    parser.add_argument('-i', '--interpolate', choices=['linear', 'step'], default='linear')
     parser.add_argument('-b', '--begin', type=Decimal, default=None, help='value to begin resampling at')
     parser.add_argument('-t', '--terminate', type=Decimal, default=None, help='value to terminate resampling at')
-    parser.add_argument('-e', '--expand', action='store_true', default=False)
+    parser.add_argument('-r', '--resample_file', type=argparse.FileType('r'), default=None, help='File to read resample points from')
+    parser.add_argument('-e', '--resample_index', type=int, default=0)
     parser.add_argument('-x', '--xdata', type=int, default=0)
     parser.add_argument('-y', '--ydata', type=int, default=1)
     parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
+    parser.add_argument('-o', '--ordered', action='store_true', default=False, help='input is sorted by group')
     args = parser.parse_args()
-    if args.begin and args.sync:
-        raise Exception('Cannot specify both sync and begin parameters')
+
+    if args.begin and args.resample_file:
+        raise Exception('Cannot specify both file and begin parameters')
+    elif args.resample_file:
+        args.resample_values = [Decimal(line.rstrip().split()[args.resample_index]) for line in args.resample_file]
+        args.resample_file.close()
     args.interpolatef = getattr(sys.modules[__name__], 'interp_'+args.interpolate)
-    
-    grouper = UnsortedInputGrouper(args.infile, ResampleGroup, args.group, args.delimiter)
-    grouper.group()
+
+    run_grouping(args.infile, ResampleGroup, args.group, args.delimiter, args.ordered)
+
