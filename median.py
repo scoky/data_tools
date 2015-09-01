f6be6c51db660266b22de17969f7b824a8025fe6
Kyle Schomp <kgs7@case.edu>
2015-09-01 08:38:55 -0400

    updated median

--- median.py
+++ median.py
@@ -10,18 +10,17 @@ from group import Group,run_grouping
 class MedianGroup(Group):
     def __init__(self, tup):
         super(MedianGroup, self).__init__(tup)
-        self.rows = [[] for i in args.columns]
+        self.rows = []
 
     def add(self, chunks):
-        vals = [findNumber(chunks[i]) for i in args.columns]
-        for v,r in zip(vals, self.rows):
-            r.append(v)
+        val = findNumber(chunks[args.column])
+        self.rows.append(val)
 
     def done(self):
         jdelim = args.delimiter if args.delimiter != None else ' '
         if len(self.tup) > 0:
             args.outfile.write(jdelim.join(self.tup) + jdelim)
-        args.outfile.write(jdelim.join([str(computeMedian(r)) for r in self.rows]) + '\n')
+        args.outfile.write(str(computeMedian(self.rows)) + '\n')
 
 def computeMedian(r):
     if len(r) % 2 == 1: # Odd length
@@ -35,7 +34,7 @@ if __name__ == "__main__":
                                      description='Compute mean of column(s)')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
+    parser.add_argument('-c', '--column', type=int, default=0)
     parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
     parser.add_argument('-o', '--ordered', action='store_true', default=False, help='input is sorted by group')
