12489ae155fc2c8cbeab9b6deb2b1df6f4286083
Kyle Schomp <kgs7@case.edu>
2015-09-06 20:07:21 -0400

    added new functionality

--- join.py
+++ join.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                                      description='Join files on column(s) to first file')
     parser.add_argument('infiles', nargs='*', type=argparse.FileType('r'), default=[sys.stdin])
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[0])
+    parser.add_argument('-c', '--columns', nargs='+', default=['0'], help='use + to specify multiple columns per file')
     parser.add_argument('-d', '--delimiter', default=None)
     parser.add_argument('-i', '--inner', action='store_true', default=False, help='inner join')
     args = parser.parse_args()
@@ -21,6 +21,10 @@ if __name__ == "__main__":
     if len(args.columns) != len(args.infiles):
         sys.stderr.write('InputError: invalid columns argument\n')
         exit()
+    cols = []
+    for c in args.columns:
+        cols.append(map(int, c.split('+')))
+    args.columns = cols
         
     jdelim = args.delimiter if args.delimiter != None else ' '
     merge = defaultdict(str)
@@ -29,7 +33,8 @@ if __name__ == "__main__":
         infile,col = tup
         for line in infile:
             line = line.rstrip()
-            chunk = line.split(args.delimiter)[col]
+            chunks = line.split(args.delimiter)
+            chunk = jdelim.join([chunks[c] for c in col])
             merge[chunk] += '#JOIN#'+str(i) + jdelim + line + jdelim
             counts[chunk] += 1
         infile.close()
@@ -37,7 +42,8 @@ if __name__ == "__main__":
     col = args.columns[0]
     for line in args.infiles[0]:
         line = line.rstrip()
-        chunk = line.split(args.delimiter)[col]
+        chunks = line.split(args.delimiter)
+        chunk = jdelim.join([chunks[c] for c in col])
         if chunk in merge:
             line += jdelim + merge[chunk]
         if not args.inner or counts[chunk] == len(args.infiles) - 1:
