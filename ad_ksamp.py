148334970166df1ba91501c98120aeec6c95de57
Kyle Schomp <kgs7@case.edu>
2015-10-10 13:49:37 -0400

    updated the ad test script

--- ad_ksamp.py
+++ ad_ksamp.py
@@ -15,9 +15,13 @@ class ADGroup(Group):
         super(ADGroup, self).__init__(tup)
         args.groups.append(self)
         self.samples = []
+        for i in range(len(args.columns)):
+            self.samples.append([])
 
     def add(self, chunks):
-        self.samples.append(float(chunks[args.column]))
+        values = [float(chunks[i]) for i in args.columns]
+        for s,v in zip(self.samples, values):
+            s.append(v)
 
     def done(self):
         pass
@@ -25,28 +29,30 @@ class ADGroup(Group):
 def AD_test(groups, outfile):
     jdelim = args.delimiter if args.delimiter != None else ' '
     for i,u in enumerate(groups):
-        if len(u.samples) < args.ignore:
-            continue
         for j,v in enumerate(groups):
-            if j >= i:
+            if j > i or (j == i and len(args.columns) == 1):
                 break
-            if len(v.samples) < args.ignore:
-                continue
-            if args.random != None:
-                verdict = False
-                for k in range(args.random):
-                    res = anderson_ksamp([random.sample(u.samples, args.subsample), random.sample(v.samples, args.subsample)])
-                    if res[0] < res[1][0]:
-                        verdict = True
-                    outfile.write(jdelim.join(u.tup + v.tup + map(str, res)) + '\n')
-                outfile.write('Verdict:' + str(verdict) + '\n')
-            else:
-                res = anderson_ksamp([u.samples, v.samples])
-                verdict = False
-                if res[0] < res[1][0]:
-                        verdict = True
-                outfile.write(jdelim.join(u.tup + v.tup + map(str, res)) + '\n')
-                outfile.write('Verdict:' + str(verdict) + '\n')
+            for x,us in enumerate(u.samples):
+                for y,vs in enumerate(v.samples):
+                    if len(vs) < args.ignore or len(us) < args.ignore:
+                        continue
+                    if j == i and y >= x:
+                        break
+                    if args.random != None:
+                        verdict = False
+                        for k in range(args.random):
+                            res = anderson_ksamp([random.sample(us, args.subsample), random.sample(vs, args.subsample)])
+                            if res[0] < res[1][0]:
+                                verdict = True
+                            outfile.write(jdelim.join(u.tup + v.tup + map(str, res)) + '\n')
+                        outfile.write('Verdict:' + str(verdict) + '\n')
+                    else:
+                        res = anderson_ksamp([us, vs])
+                        verdict = False
+                        if res[0] < res[1][0]:
+                                verdict = True
+                        outfile.write(jdelim.join(u.tup + v.tup + map(str, res)) + '\n')
+                        outfile.write('Verdict:' + str(verdict) + '\n')
 
 if __name__ == "__main__":
     # set up command line args
@@ -55,8 +61,8 @@ if __name__ == "__main__":
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     parser.add_argument('-i', '--ignore', type=int, default=0, help='filter groups with less than threshold number of samples')
-    parser.add_argument('-c', '--column', type=int, default=1)
-    parser.add_argument('-g', '--group', nargs='+', type=int, default=[0])
+    parser.add_argument('-c', '--columns', nargs='+', type=int, default=[1])
+    parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
     parser.add_argument('-d', '--delimiter', default=None)
     parser.add_argument('-r', '--random', default=None, type=int, help='perform on r random subsamples')
     parser.add_argument('-s', '--subsample', default=100, type=int, help='subsample size')
