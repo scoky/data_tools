538dc6d903d7a4435cc72bdce67e739fa381281c
Kyle Schomp <kgs7@case.edu>
2016-04-06 21:14:36 -0400

    updated dbscan to new format

--- dbscan.py
+++ dbscan.py
@@ -1,39 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import os
 import sys
 import argparse
 from sklearn.cluster import DBSCAN
-from input_handling import findNumber,FileReader,Header
+from input_handling import findNumber,ParameterParser
 from group import Group,run_grouping
 
-def scan_matrix():
-    labelToPos = {}
-    matrixDict = {}
-    count = 0
-    for line in args.infile:
-        chunks = line.rstrip().split(args.delimiter)
+class OfflineDBSCANGroup(Group):
+    def __init__(self, tup):
+        super(OfflineDBSCANGroup, self).__init__(tup)
+        self.labelToPos = {}
+        self.matrixDict = {}
+        self.count = 0
+
+    def add(self, chunks):
         first = chunks[args.first]
         second = chunks[args.second]
         distance = float(findNumber(chunks[args.range]))
-        if first not in labelToPos:
-            labelToPos[first] = count
-            count += 1
-        if second not in labelToPos:
-            labelToPos[second] = count
-            count += 1
-        matrixDict[(labelToPos[first], labelToPos[second])] = distance
+        if first not in self.labelToPos:
+            self.labelToPos[first] = self.count
+            self.count += 1
+        if second not in self.labelToPos:
+            self.labelToPos[second] = self.count
+            self.count += 1
+        self.matrixDict[(self.labelToPos[first], self.labelToPos[second])] = distance
 
-    matrix = [[0]*count for i in range(count)]
-    for keys,distance in matrixDict.iteritems():
-        matrix[keys[0]][keys[1]] = distance
-        matrix[keys[1]][keys[0]] = distance
-        
-    db = DBSCAN(eps=args.epsilon, metric='precomputed', min_samples=args.min_samples)
-    output = db.fit(matrix)
-    
-    for label,i in labelToPos.iteritems():
-        args.outfile.write(label + args.jdelim + str(output.labels_[i]) + '\n')
+    def done(self):
+        matrix = [[0]*self.count for i in range(self.count)]
+        for keys,distance in self.matrixDict.iteritems():
+            matrix[keys[0]][keys[1]] = distance
+            matrix[keys[1]][keys[0]] = distance
+
+        db = DBSCAN(eps=args.epsilon, metric='precomputed', min_samples=args.min_samples)
+        output = db.fit(matrix)
+
+        for label,i in self.labelToPos.iteritems():
+            args.outfile.write(self.tup + [label, output.labels_[i]])
 
 def dist(tx, ty):
     return abs(tx - ty)
@@ -70,13 +73,13 @@ class Neighbor(object):
             yield i
             i = i.next
 
-class DBSCANGroup(Group):
+class OnlineDBSCANGroup(Group):
     def __init__(self, tup):
-        super(DBSCANGroup, self).__init__(tup)
+        super(OnlineDBSCANGroup, self).__init__(tup)
         self.head = self.tail = None
 
     def add(self, chunks):
-        n = Neighbor(float(findNumber(chunks[args.range])), args.jdelim.join(chunks))
+        n = Neighbor(float(findNumber(chunks[args.range])), chunks)
 
         # Update neighbor counts
         oldest = None
@@ -113,51 +116,36 @@ class DBSCANGroup(Group):
         
     def empty(self):
         for nn in Neighbor.iterate_forward(self.head):
-            args.outfile.write(nn.line + args.jdelim + str(nn.label) + '\n')
+            args.outfile.write(nn.line + [nn.label])
         self.head = self.tail = None
 
     def done(self):
         self.empty()
 
 if __name__ == "__main__":
-    # set up command line args
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
-                                     description='Cluster input using the DBSCAN algorithm')
-    parser.add_argument('infile', nargs='?', default=sys.stdin)
-    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-f', '--first', default=0, help='first key column')
-    parser.add_argument('-s', '--second', default=1, help='second key column')
-    parser.add_argument('-r', '--range', default=2, help='distance between first and second keys')
-    parser.add_argument('-l', '--online', action='store_true', default=False, help='changes meaning of range parameter to a monotonically increasing position value')
-    parser.add_argument('-e', '--epsilon', type=float, default=0.5)
-    parser.add_argument('-m', '--min_samples', type=int, default=5)
-    parser.add_argument('-d', '--delimiter', default=None)
-    parser.add_argument('-g', '--group', nargs='+', type=int, default=[])
-    parser.add_argument('-o', '--ordered', action='store_true', default=False, help='input is sorted by group')
-    args = parser.parse_args()
-    args.infile = FileReader(args.infile)
-
-    # Get the header from the input file if there is one
-    args.inheader = args.infile.Header()
-    # Setup output header
+    pp = ParameterParser('Cluster input using the DBSCAN algorithm', columns = 0, labels = [None], append = False)
+    pp.parser.add_argument('--online', action='store_true', default=False, help='changes meaning of range parameter to a monotonically increasing position value')
+    pp.parser.add_argument('-f', '--first', default='0', help='first key column')
+    pp.parser.add_argument('-s', '--second', default='1', help='second key column (offline only)')
+    pp.parser.add_argument('-r', '--range', default='2', help='column with distance')
+    pp.parser.add_argument('-e', '--epsilon', type=float, default=0.5)
+    pp.parser.add_argument('-m', '--min_samples', type=int, default=5)
+    args = pp.parseArgs()
     if args.online:
-        args.outheader = args.inheader.copy()
-        args.group = args.inheader.indexes(args.group)
-    else:
-        args.outheader = Header()
-        args.outheader.addCol(args.inheader.name(args.first))
-        args.first = args.inheader.index(args.first)
-        args.second = args.inheader.index(args.second)
-    args.outheader.addCol('cluster')
-    # Write output header
-    args.outfile.write(args.outheader.value())
-    # Get columns for use in computation
-    args.range = args.inheader.index(args.range)
+        args.append = True
+    if not any(args.labels):
+        if args.online:
+            args.labels = ['label']
+        else:
+            args.labels = [args.infile.header.name(args.first), 'label']
+    args = pp.getArgs(args)
+    args.first = args.infile.header.index(args.first)
+    args.second = args.infile.header.index(args.second)
+    args.range = args.infile.header.index(args.range)
 
-    args.jdelim = args.delimiter if args.delimiter != None else ' '
     if args.online:
         args.label = 0
-        run_grouping(args.infile, DBSCANGroup, args.group, args.delimiter, args.ordered)
+        run_grouping(args.infile, OnlineDBSCANGroup, args.group, args.ordered)
     else:
-        scan_matrix()
+        run_grouping(args.infile, OfflineDBSCANGroup, args.group, args.ordered)
 
