b62104ae95acc667925d3e2ee4860c42f7090027
Kyle Schomp <kgs7@case.edu>
2016-04-09 10:10:36 -0400

    updated another script

--- concat.py
+++ concat.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import os
 import sys
 import argparse
-from input_handling import FileReader,Header
+from input_handling import ParameterParser
 
 def concat_rows():
-    # Write combined header
-    outheader = Header()
-    for inheader in args.inheaders:
-        outheader.addCols(inheader)
-    args.outfile.write(outheader.value())
-
+    for infile in args.infiles:
+        args.outfile.header.addCols(infile.header.columns)
     while len(args.infiles) > 0:
-        lines = [infile.readline().rstrip() for infile in args.infiles]
+        lines = [infile.readline() for infile in args.infiles]
         out = []
         remove = []
         for i,line in enumerate(lines):
             if line:
-                out.append(line)
+                out.extend(line)
             else:
                 remove.append(i)
         if 0 < len(remove) < len(args.infiles):
@@ -26,35 +22,21 @@ def concat_rows():
         for i in reversed(remove):
             del args.infiles[i]
         if len(out) > 0:
-            args.outfile.write(args.jdelim.join(out) + '\n')
+            args.outfile.write(out)
 
 def concat_cols():
-    # Write first header
-    header = Header()
-    for inheader in args.inheaders:
-        if inheader and len(inheader) > 0:
-            header = inheader
-            break
-    args.outfile.write(header.value())
-
+    args.outfile.header.addCols(args.infiles[0].header.columns)
     for infile in args.infiles:
         for line in infile:
             args.outfile.write(line)
         infile.close()
 
 if __name__ == "__main__":
-    # set up command line args
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
-                                     description='Concatenate files')
-    parser.add_argument('infiles', nargs='+', default=[sys.stdin])
-    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
-    parser.add_argument('-m', '--method', default='columns', choices=['rows', 'columns'])
-    parser.add_argument('-d', '--delimiter', default=None)
-    args = parser.parse_args()
+    pp = ParameterParser('Concatenate files', columns = 0, group = False, ordered = False, infiles = '*', append = False)
+    pp.parser.add_argument('-m', '--method', default='columns', choices=['rows', 'columns'])
+    args = pp.parseArgs()
+    args = pp.getArgs(args)
 
-    args.jdelim = args.delimiter if args.delimiter != None else ' '
-    args.infiles = [FileReader(infile) for infile in args.infiles]
-    args.inheaders = [infile.Header() for infile in args.infiles]
     if args.method == 'columns':
         concat_cols()
     else:
