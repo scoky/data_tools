7016cb566da78202e456eeb1430d20664f5493ce
Kyle Schomp <kschomp@akamai.com>
2016-06-10 08:54:21 -0400

    fixed fuzzing

--- bin.py
+++ bin.py
@@ -4,13 +4,15 @@ import os
 import sys
 import argparse
 from collections import defaultdict
-from toollib.files import ParameterParser
+from toollib.files import ParameterParser,findNumber
 from toollib.group import Group,run_grouping
 
 class BinGroup(Group):
     def __init__(self, tup):
         super(BinGroup, self).__init__(tup)
-        self.key = args.fuzz(tup)
+        if args.numerical:
+            tup = [findNumber(t) for t in tup]
+        self.key = tuple([args.fuzz(t) for t in tup])
 
     def add(self, chunks):
         args.bins[self.key] += 1
@@ -21,6 +23,8 @@ class BinGroup(Group):
 class SimilarGroup(Group):
     def __init__(self, tup):
         super(SimilarGroup, self).__init__(tup)
+        if args.numerical:
+            tup = [findNumber(t) for t in tup]
         self.key = tuple(self.tup)
         for k in args.bins:
             if args.similar(self.key, k):
@@ -33,14 +37,15 @@ class SimilarGroup(Group):
     def done(self):
         pass
 
-# Default handling of input value    
+# Default handling of input value
 def nofuzz(v):
-    return tuple(v)
+    return v
 
 if __name__ == "__main__":
     pp = ParameterParser('Compute bins', columns = 0, labels = [None], append = False)
     pp.parser.add_argument('-f', '--fuzz', default=None, help='lambda specifying fuzz for bins')
     pp.parser.add_argument('-s', '--similar', default=None, help='lambda specifying similarity between two arguments')
+    pp.parser.add_argument('-n', '--numerical', action='store_true', default=False, help='treat the group values as numbers')
     args = pp.parseArgs()
     if not any(args.labels):
         args.labels = ['_'.join(args.group_names + ['bin'])]
@@ -61,4 +66,3 @@ if __name__ == "__main__":
     run_grouping(args.infile, cls, args.group, args.ordered)
     for k,v in args.bins.iteritems():
         args.outfile.write(list(k) + [v])
-
