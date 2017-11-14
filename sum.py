6842508c95a5a5de6c3e9708bb8eec0697b582a8
Kyle Schomp <kschomp@akamai.com>
2017-11-14 09:06:21 -0500

    added multirow sums to sum.py

--- sum.py
+++ sum.py
@@ -9,19 +9,20 @@ from toollib.group import Group,run_grouping
 class SumGroup(Group):
     def __init__(self, tup):
         super(SumGroup, self).__init__(tup)
-        self.sums = 0
+        self.sums = [0]*len(args.columns)
 
     def add(self, chunks):
-        self.sums += findNumber(chunks[args.column])
+        for i,c in args.columns:
+            self.sums[i] += findNumber(chunks[c])
 
     def done(self):
-        args.outfile.write(self.tup + [self.sums])
+        args.outfile.write(self.tup + self.sums)
 
 if __name__ == "__main__":
-    pp = ParameterParser('Sum of column', columns = 1, append = False, labels = [None])
+    pp = ParameterParser('Sum of column', columns = '*', append = False, labels = [None])
     args = pp.parseArgs()
     if not any(args.labels):
-        args.labels = [args.column_name + '_sum']
+        args.labels = [cn + '_sum' for cn in args.columns_names]
     args = pp.getArgs(args)
 
     run_grouping(args.infile, SumGroup, args.group, args.ordered)
