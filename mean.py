2f33738d74edaa0519e0bd9510956c35fa54d0fa
Kyle Schomp <kschomp@akamai.com>
2017-11-28 13:55:11 -0500

    made mean.py multi column

--- mean.py
+++ mean.py
@@ -10,50 +10,55 @@ from toollib.group import Group,run_grouping
 class MeanGroup(Group):
     def __init__(self, tup):
         super(MeanGroup, self).__init__(tup)
-        self.sums = 0
-        self.count = 0
-        self.add = self.addVal if args.bin is None else self.addBin
+        self.sums = [0]*len(args.columns)
+        self.count = [0]*len(args.columns)
+        self.add = self.addVal if args.bins is None else self.addBin
 
     def addVal(self, chunks):
-        self.sums += findNumber(chunks[args.column])
-        self.count += 1
+        for i,c in enumerate(args.columns):
+            self.sums[i] += findNumber(chunks[c])
+            self.count[i] += 1
         
     def addBin(self, chunks):
-        b = findNumber(chunks[args.bin])
-        self.sums += b*findNumber(chunks[args.column])
-        self.count += b
+        for i,c in enumerate(args.columns):
+            b = findNumber(chunks[args.bins[i]])
+            self.sums[i] += b*findNumber(chunks[c])
+            self.count[i] += b
 
     def done(self):
-        args.outfile.write(self.tup + [self.sums / self.count])
+        args.outfile.write(self.tup + [s / c for s,c in zip(self.sums, self.count)])
 
 class GeometricGroup(Group):
     def __init__(self, tup):
         super(GeometricGroup, self).__init__(tup)
-        self.sums = 1
-        self.count = 0
-        self.add = self.addVal if args.bin is None else self.addBin
+        self.sums = [1]*len(args.columns)
+        self.count = [0]*len(args.columns)
+        self.add = self.addVal if args.bins is None else self.addBin
 
     def addVal(self, chunks):
-        self.sums *= findNumber(chunks[args.column])
-        self.count += 1
+        for i,c in enumerate(args.columns):
+            self.sums[i] *= findNumber(chunks[c])
+            self.count[i] += 1
         
     def addBin(self, chunks):
-        b = findNumber(chunks[args.bin])
-        self.sums *= pow(findNumber(chunks[args.column]), b)
-        self.count += b
+        for i,c in enumerate(args.columns):
+            b = findNumber(chunks[args.bins[i]])
+            self.sums[i] *= pow(findNumber(chunks[c]), b)
+            self.count[i] += b
 
     def done(self):
-        args.outfile.write(self.tup + [pow(self.sums, 1 / self.count)])
+        args.outfile.write(self.tup + [pow(s, 1 / c) for s,c zip(self.sums, self.count)])
 
 if __name__ == "__main__":
-    pp = ParameterParser('Compute mean of column', columns = 1, labels = [None])
-    pp.parser.add_argument('-b', '--bin', default=None)
+    pp = ParameterParser('Compute mean of columns', columns = '*', labels = [None])
+    pp.parser.add_argument('-b', '--bins', default = None, nargs='+')
     pp.parser.add_argument('-e', '--geometric', action='store_true', default=False, help='compute geometric mean')
     args = pp.parseArgs()
     if not any(args.labels):
-        args.labels = [args.column_name + ('_gmean' if args.geometric else '_mean')]
+        args.labels = [cn + ('_gmean' if args.geometric else '_mean') for cn in args.columns_names]
     args = pp.getArgs(args)
-    args.bin = args.infile.header.index(args.bin)
+    if args.bin is not None:
+        args.bin = args.infile.header.indexes(args.bin)
 
     if args.geometric:
         cls = GeometricGroup
