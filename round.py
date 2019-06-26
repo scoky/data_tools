d2f9bad5b80336c09bc38023a5b06801275eacf1
Kyle Schomp <kschomp@akamai.com>
2019-06-26 14:42:59 +0100

    making roung script work with multiple rows

--- round.py
+++ round.py
@@ -10,17 +10,11 @@ from toollib.group import Group,run_grouping
 class RoundGroup(Group):
     def __init__(self, tup):
         super(RoundGroup, self).__init__(tup)
-        self.row = []
-        if args.append:
-            self.add = self.addAppend
 
     def add(self, chunks):
-        val = args.quantF(args.binF(findNumber(chunks[args.column]))) + zero
-        args.outfile.write([val])
-
-    def addAppend(self, chunks):
-        val = args.quantF(args.binF(findNumber(chunks[args.column]))) + zero
-        args.outfile.write(chunks + [val])
+        for c in args.columns:
+            chunks[c] = args.quantF(args.binF(findNumber(chunks[c]))) + zero
+        args.outfile.write(chunks)
 
     def done(self):
         pass
@@ -36,13 +30,11 @@ def binify(val):
     return (val / args.bin).to_integral_exact(rounding=ROUND_FLOOR) * args.bin
 
 if __name__ == "__main__":
-    pp = ParameterParser('Compute pdf', columns = 1, labels = [None], group = False, ordered = False)
+    pp = ParameterParser('Compute pdf', columns = '*', labels = False, group = False, ordered = False, append = False)
     pp.parser.add_argument('-q', '--quantize', type=Decimal, default=None, help='fixed exponent (e.g., 10, 1, 0.1)')
     pp.parser.add_argument('-s', '--significantDigits', type=int, default=None, help='number of significant digits')
     pp.parser.add_argument('-b', '--bin', type=Decimal, default=None, help='fit into bins, applies the formula: f(x) = floor(x / b) * b')
     args = pp.parseArgs()
-    if not any(args.labels):
-        args.labels = [args.column_name + '_round']
     args = pp.getArgs(args)
     if args.significantDigits is not None:
         getcontext().prec = args.significantDigits
