b3dbec9411c67780c6f0da40574df68aeacc0fda
Kyle Schomp <kyle.schomp@gmail.com>
2022-07-14 08:26:58 -0400

    adding reverting of collect

--- data_tools/collect.py
+++ data_tools/collect.py
@@ -19,11 +19,27 @@ class CollectGroup(Group):
     def done(self):
         args.outfile.write(self.tup + [args.join.join(c) for c in self.collects])
 
+class ReverseCollectGroup(Group):
+    def __init__(self, tup):
+        super(ReverseCollectGroup, self).__init__(tup)
+
+    def add(self, chunks):
+        for c in args.columns:
+            for cc in chunks[c].split(args.join):
+                if args.append:
+                    args.outfile.write(chunks + [cc])
+                else:
+                    args.outfile.write([cc])
+
 if __name__ == "__main__":
     pp = ParameterParser('Compute mean of columns', columns = '*', labels = [None])
     pp.parser.add_argument('-j', '--join', default=' ', help='character to use to join values together')
+    pp.parser.add_argument('-r', '--reverse', action='store_true', help='reverse the collection')
     args = pp.parseArgs()
     if not any(args.labels):
         args.labels = [cn + '_collect' for cn in args.columns_names]
     args = pp.getArgs(args)
-    run_grouping(args.infile, CollectGroup, args.group, args.ordered)
+    if args.reverse:
+        run_grouping(args.infile, ReverseCollectGroup, args.group, args.ordered)
+    else:
+        run_grouping(args.infile, CollectGroup, args.group, args.ordered)
