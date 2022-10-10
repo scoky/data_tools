9847b69be10fc58cafd0c74a0d824978adcd9686
Kyle Schomp <kyle.schomp@gmail.com>
2022-10-10 14:48:03 -0400

    adding more options for display purposes

--- scripts/tab.py
+++ scripts/tab.py
@@ -4,7 +4,7 @@ import os
 import sys
 from data_tools.files import ParameterParser
 
-def printCols(data, headers = None, stream = sys.stdout, min_lengths = None, precision = None):
+def printCols(data, headers = None, stream = sys.stdout, min_lengths = None, precision = None, commas = False):
    rows = []
    if headers is not None:
       rows.append(headers)
@@ -16,12 +16,13 @@ def printCols(data, headers = None, stream = sys.stdout, min_lengths = None, pre
    # Set the initial lengths
    lengths = min_lengths if min_lengths is not None else [0] * len(rows[0])
    # Update precision of floats
-   if not precision is None:
-      newRows = []
-      for row in rows:
-         row = [('{:.' + str(precision) + 'f}').format(float(v)) if isFloatingPoint(v) else v for v in row]
-         newRows.append(row)
-      rows = newRows
+   float_fmt = '{:' + (',' if commas else '') + ('' if precision is None else ('.' + str(precision) + 'f')) + '}'
+   int_fmt = '{:' + (',' if commas else '') + '}'
+   newRows = []
+   for row in rows:
+      row = [int_fmt.format(int(v)) if isInt(v) else (float_fmt.format(float(v)) if isFloatingPoint(v) else v) for v in row]
+      newRows.append(row)
+   rows = newRows
    # Run once over the rows to find the maximum column lengths
    for row in rows:
       lengths = [max(lengths[c], len(v)) for c,v in enumerate(row)]
@@ -31,19 +32,20 @@ def printCols(data, headers = None, stream = sys.stdout, min_lengths = None, pre
 
    return lengths
 
+def isInt(val):
+   try:
+      int(val)
+      return True
+   except ValueError:
+      return False
+
 def isFloatingPoint(val):
    try:
       float(val)
-   except ValueError:
-      # Can be parsed as a float
-      return False
-   try:
-      int(val)
-      # Can be parsed as an int
-      return False
-   except ValueError:
-      # Can't be parsed as an int
       return True
+   except ValueError:
+      # Can't be parsed as a float
+      return False
 
 def glob(instream, header, rows_per_glob):
    rows = []
@@ -58,12 +60,13 @@ def glob(instream, header, rows_per_glob):
       yield rows
 
 if __name__ == "__main__":
-    pp = ParameterParser('Pretty print file', columns = 0, group = False, append = False, ordered = False)
-    pp.parser.add_argument('-k', '--k', type = int, default = 30, help = 'frequency in rows to recalculate column width')
-    pp.parser.add_argument('-p', '--precision', type = int, default = 2, help = 'for floating point numbers, the number of digits after the decimal point to display (0 disables')
-    args = pp.parseArgs()
+   pp = ParameterParser('Pretty print file', columns = 0, group = False, append = False, ordered = False)
+   pp.parser.add_argument('-k', '--k', type = int, default = 30, help = 'frequency in rows to recalculate column width')
+   pp.parser.add_argument('-p', '--precision', type = int, default = 2, help = 'for floating point numbers, the number of digits after the decimal point to display (0 disables)')
+   pp.parser.add_argument('-c', '--commas', action='store_true', default=False, help = 'add commas to numbers')
+   args = pp.parseArgs()
 
-    header = args.infile.header
-    length = None
-    for rows in glob(args.infile, header, args.k):
-        length = printCols(rows, min_lengths = length, precision = args.precision if args.precision > 0 else None)
+   header = args.infile.header
+   length = None
+   for rows in glob(args.infile, header, args.k):
+      length = printCols(rows, min_lengths = length, precision = args.precision if args.precision > 0 else None, commas = args.commas)
