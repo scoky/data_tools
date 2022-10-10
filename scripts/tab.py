1b87e4aceba768268fba691e1826bfb24cb4b492
Kyle Schomp <kyle.schomp@gmail.com>
2022-10-10 09:51:57 -0400

    adding precision to table display

--- scripts/tab.py
+++ scripts/tab.py
@@ -4,7 +4,7 @@ import os
 import sys
 from data_tools.files import ParameterParser
 
-def printCols(data, headers = None, stream = sys.stdout, min_lengths = None):
+def printCols(data, headers = None, stream = sys.stdout, min_lengths = None, precision = None):
    rows = []
    if headers is not None:
       rows.append(headers)
@@ -15,6 +15,13 @@ def printCols(data, headers = None, stream = sys.stdout, min_lengths = None):
 
    # Set the initial lengths
    lengths = min_lengths if min_lengths is not None else [0] * len(rows[0])
+   # Update precision of floats
+   if not precision is None:
+      newRows = []
+      for row in rows:
+         row = [('{:.' + str(precision) + 'f}').format(float(v)) if isFloatingPoint(v) else v for v in row]
+         newRows.append(row)
+      rows = newRows
    # Run once over the rows to find the maximum column lengths
    for row in rows:
       lengths = [max(lengths[c], len(v)) for c,v in enumerate(row)]
@@ -24,9 +31,23 @@ def printCols(data, headers = None, stream = sys.stdout, min_lengths = None):
 
    return lengths
 
+def isFloatingPoint(val):
+   try:
+      float(val)
+   except ValueError:
+      # Can be parsed as a float
+      return False
+   try:
+      int(val)
+      # Can be parsed as an int
+      return False
+   except ValueError:
+      # Can't be parsed as an int
+      return True
+
 def glob(instream, header, rows_per_glob):
    rows = []
-   if header is not None:
+   if len(header) > 0:
       rows.append(header)
    for row in instream:
       if len(rows) == rows_per_glob:
@@ -39,9 +60,10 @@ def glob(instream, header, rows_per_glob):
 if __name__ == "__main__":
     pp = ParameterParser('Pretty print file', columns = 0, group = False, append = False, ordered = False)
     pp.parser.add_argument('-k', '--k', type = int, default = 30, help = 'frequency in rows to recalculate column width')
+    pp.parser.add_argument('-p', '--precision', type = int, default = 2, help = 'for floating point numbers, the number of digits after the decimal point to display (0 disables')
     args = pp.parseArgs()
 
-    header = args.infile.header if args.header else None
+    header = args.infile.header
     length = None
     for rows in glob(args.infile, header, args.k):
-        length = printCols(rows, min_lengths = length)
+        length = printCols(rows, min_lengths = length, precision = args.precision if args.precision > 0 else None)
